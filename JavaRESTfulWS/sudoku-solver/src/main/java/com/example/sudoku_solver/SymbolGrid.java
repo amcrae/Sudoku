9a76f22a25925d84fa5141f896db340e010c6ed2
Andrew McRae <716783+amcrae@users.noreply.github.com>
2024-10-11 13:22:45 +1000

    Add factory method

--- JavaRESTfulWS/sudoku-solver/src/main/java/com/example/sudoku_solver/SymbolGrid.java
+++ JavaRESTfulWS/sudoku-solver/src/main/java/com/example/sudoku_solver/SymbolGrid.java
@@ -6,4 +6,15 @@ import java.util.ArrayList;
  * */
 public class SymbolGrid extends ArrayList<ArrayList<String>> {
 	
+	public static SymbolGrid fromStringArrays(String[][] a) {
+		SymbolGrid g = new SymbolGrid();
+		for (int y = 0; y<a.length; y++) {
+			ArrayList<String> row = new ArrayList<>();
+			for (int x =0; x<a[y].length; x++) {
+				row.add(a[y][x]);
+			}
+			g.add(row);
+		}
+		return g;
+	}
 }
